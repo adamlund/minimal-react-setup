93cd1b13e47b35978058d11c7ecb556a99281692
Adam Lund <lundmn@gmail.com>
2019-03-31 10:10:38 -0500

    Unit test for async load component

--- __test__/jestsetup.js
+++ __test__/jestsetup.js
@@ -2,3 +2,8 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+// https://www.npmjs.com/package/jest-fetch-mock#simple-mock-and-assert
+global.fetch = require('jest-fetch-mock');
+
+global.delay = ms => new Promise(resolve => setTimeout(resolve, ms));
